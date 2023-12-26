8ebbbc163535d0fe9305df5d80f352cb71e4b4d6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-12-26 13:18:15 +0530

    just11

--- env_dev/main.tfvars
+++ env_dev/main.tfvars
@@ -1,8 +1,9 @@
 env = "dev"
 
-vpc= {
+
+vpc = {
   main = {
-    cidr_block= "10.0.0.0/16"
+    cidr_block   = "10.0.0.0/16"
     subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
   }
 }
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "network_vpc" {
-  source     = "github.com/csp0403/tf_module_vpc.git"
-  env        = var.env
+module "network" {
+  source         = "github.com/raghudevopsb70/tf-module-vpc"
+  env            = var.env
+  default_vpc_id = var.default_vpc_id
 
-  for_each   = var.vpc
-  cidr_block = each.value.cidr_block
+  for_each     = var.vpc
+  cidr_block   = each.value.cidr_block
   subnets_cidr = each.value.subnets_cidr
-
 }
